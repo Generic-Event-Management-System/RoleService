df1b396dac05553fb622df7e3c394765b0d7586a
alikazmi181293 <syed.ali.hussnain.kazmi@gmail.com>
2024-11-08 00:16:08 +0200

    Added database context and add registered database context in program.cs file. I have also added connection string in the app.setting

---
+++ RoleService/Persistence/RoleDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RoleService.Models.Entities;
+
+namespace RoleService.Persistence
+{
+    public class RoleDbContext : DbContext
+    {
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+
+        public RoleDbContext(DbContextOptions<RoleDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- RoleService/Program.cs
+++ RoleService/Program.cs
@@ -1,4 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using RoleService.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<RoleDbContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 
