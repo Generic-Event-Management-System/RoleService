3f3ed0d97bf1c07069bb6e478fd849092aac9286
alikazmi181293 <syed.ali.hussnain.kazmi@gmail.com>
2024-11-08 01:22:59 +0200

    Added role response dto to only display relavent response to the user.

---
+++ RoleService/Models/Dto/RoleResponseDto.cs
@@ -0,0 +1,11 @@
+﻿using RoleService.Models.Entities;
+
+namespace RoleService.Models.Dto
+{
+    public class RoleResponseDto
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+    }
+}
