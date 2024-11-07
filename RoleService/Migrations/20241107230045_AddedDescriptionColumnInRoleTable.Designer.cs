8017d1e8a367e8d3ebd775e1585209018651624a
alikazmi181293 <syed.ali.hussnain.kazmi@gmail.com>
2024-11-08 01:02:38 +0200

    Added description property in the role class and added description column migration in the project

--- RoleService/Migrations/20241107230045_AddedDescriptionColumnInRoleTable.Designer.cs
+++ RoleService/Migrations/20241107230045_AddedDescriptionColumnInRoleTable.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RoleService.Persistence;
 
@@ -10,9 +11,11 @@ using RoleService.Persistence;
 namespace RoleService.Migrations
 {
     [DbContext(typeof(RoleDbContext))]
-    partial class RoleDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241107230045_AddedDescriptionColumnInRoleTable")]
+    partial class AddedDescriptionColumnInRoleTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
