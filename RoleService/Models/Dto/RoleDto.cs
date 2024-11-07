86129a3b70b5e5925543eef3fbf34fc36e295024
alikazmi181293 <syed.ali.hussnain.kazmi@gmail.com>
2024-11-08 01:12:58 +0200

    Added role dto entity in the project

---
+++ RoleService/Models/Dto/RoleDto.cs
@@ -0,0 +1,10 @@
+﻿using RoleService.Models.Entities;
+
+namespace RoleService.Models.Dto
+{
+    public class RoleDto
+    {
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+    }
+}
