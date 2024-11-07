ef685fe08a42a0a678f8323c3178a4e2bfaa0ffb
alikazmi181293 <syed.ali.hussnain.kazmi@gmail.com>
2024-11-08 01:12:39 +0200

    Registered automapper in the program.cs file

--- RoleService/Program.cs
+++ RoleService/Program.cs
@@ -8,6 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<RoleDbContext>(options => 
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddAutoMapper(typeof(Program));
+
 // Add services to the container.
 builder.Services.AddScoped<IRolesService, RolesService>();
 
