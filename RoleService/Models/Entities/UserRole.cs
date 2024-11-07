02cca59b3c0dbfb3e6757d8abca90477f7932646
alikazmi181293 <syed.ali.hussnain.kazmi@gmail.com>
2024-11-08 00:26:34 +0200

    Added Role entity in the UserRole entity

--- RoleService/Models/Entities/UserRole.cs
+++ RoleService/Models/Entities/UserRole.cs
@@ -3,7 +3,7 @@
     public class UserRole
     {
         public int Id { get; set; }
-        public int UserId { get; set; }
-        public int RoleId { get; set; }
+        public required int UserId { get; set; }
+        public required Role Role { get; set; }
     }
 }
