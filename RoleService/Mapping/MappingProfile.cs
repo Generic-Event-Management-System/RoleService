c8dc1d58fec6532d0caa7966808158b03039ba33
alikazmi181293 <syed.ali.hussnain.kazmi@gmail.com>
2024-11-08 01:13:34 +0200

    Added mapping profile to map role to role dto entity

---
+++ RoleService/Mapping/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using RoleService.Models.Dto;
+using RoleService.Models.Entities;
+
+namespace RoleService.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile() 
+        {
+            CreateMap<Role, RoleDto>();
+            CreateMap<RoleDto, Role>();
+        }
+    }
+}
